8f707ddbc2a51b1a9c14573a60fd22ec3364bac0
obaidmuneer <96944978+obaidmuneer@users.noreply.github.com>
2022-09-11 14:31:36 +0500

    code optimized

--- app.js
+++ app.js
@@ -1,67 +1,55 @@
-
 let item = document.querySelector('#item')
 let result = document.querySelector('#result')
+let addBtn = document.querySelector('#addBtn')
+let todo = document.querySelector('#todo')
+
 let list = []
 
-function add() {
-    list.push(item.value)
+function addItem(index) {
+    if (!item.value) {
+        return
+    }
+    index > -1 ? update(index) : list.push(item.value)
     item.value = ''
-    // console.log(list);
-    show()
+    showItems()
 }
 
-function show() {
+function showItems() {
     result.innerHTML = ''
     list.map((item, index) => {
-        // console.log(item);
-        let edit = `<input class="btn button edit" type="button" onclick="edit(${index})" value="Edit">`
-        let dlt = `<input class="btn button delete" type="button" onclick="dlt(${index})" value="Delete">`
+        let edit = `<input class="btn button edit" type="button" onclick="editItem(${index})" value="Edit">`
+        let dlt = `<input class="btn button delete" type="button" onclick="dltItem(${index})" value="Delete">`
         result.innerHTML += `<li> <div class="list" > ${item} </div> ${edit} ${dlt}</li>`
     })
 }
 
-function update(index) {
-    let updatebtn = document.querySelector('#updatebtn')
-    list[index] = item.value
-    console.log(list);
-    show()
-    item.value = ''
-    updatebtn.remove()
-}
-
-function updateBtn(index) {
-    let todo = document.querySelector('#todo')
-
-    for (let i = 0; i < todo.children.length; i++) {
-        const element = todo.children[i];
-        if (element.value === 'Update') {
-            return
+function formSetting(index) {
+    if (index > -1) {
+        addBtn.innerHTML = 'Update'
+        todo.onsubmit = function () {
+            addItem(index);
+            return false
+        }
+    } else {
+        addBtn.innerHTML = 'Add'
+        todo.onsubmit = function () {
+            addItem();
+            return false
         }
     }
-    let button = `<input type="button" class="btn update button" id="updatebtn" onclick="update(${index})" value="Update">`
-    todo.insertAdjacentHTML('beforeend', button)
-
-    // const button = document.createElement('input')
-    // button.type = 'button';
-    // button.value = 'update';
-    // button.id = 'btn';
-    // button.onclick = function () {
-    //     console.log(index);
-    //     update(index)
-
-    // }
-    // todo.appendChild(button)
 }
 
-function edit(index) {
-    console.log(index);
+function update(index) {
+    list[index] = item.value
+    formSetting()
+}
+
+function editItem(index) {
     item.value = list[index]
-    updateBtn(index)
+    formSetting(index)
 }
 
-
-
-function dlt(index) {
+function dltItem(index) {
     list.splice(index, 1)
-    show()
+    showItems()
 }
